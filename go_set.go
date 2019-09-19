ef448a64e23206ee48dcfadb34358d819146ef27
Raghu <rp4raghuprasad@gmail.com>
2019-09-19 06:25:29 -0700

    sane implementation

--- go_set.go
+++ go_set.go
@@ -1,18 +1,51 @@
 package go_set
 
-// Set returns set from list
 type Set struct {
-	elems []int
+	elems    []int
+	elemsMap map[int]bool
 }
 
-func (s *Set) Elements() []int {
-	tmpMap := make(map[int]bool)
-	outList := []int{}
-	for _, v := range s.elems {
-		if _, ok := tmpMap[v]; !ok {
-			tmpMap[v] = true
-			outList = append(outList, v)
+func NewSet(elems []int) *Set {
+	s := Set{}
+	s.elemsMap = make(map[int]bool)
+	for _, e := range elems {
+		if _, ok := s.elemsMap[e]; !ok {
+			s.elemsMap[e] = true
+			s.elems = append(s.elems, e)
 		}
 	}
-	return outList
+	return &s
+}
+
+func (s Set) Val() []int {
+	return s.elems
+}
+
+func (s *Set) Add(e int) {
+	if _, ok := s.elemsMap[e]; !ok {
+		s.elemsMap[e] = true
+		s.elems = append(s.elems, e)
+	}
+}
+
+func (s1 *Set) Update(s2 Set) {
+	for _, e := range s2.Val() {
+		if _, ok := s1.elemsMap[e]; !ok {
+			s1.elemsMap[e] = true
+			s1.elems = append(s1.elems, e)
+		}
+	}
+}
+
+func (s1 Set) Equals(s2 Set) bool {
+	if len(s1.Val()) != len(s2.Val()) {
+		return false
+	}
+
+	for _, e := range s2.Val() {
+		if _, ok := s1.elemsMap[e]; !ok {
+			return false
+		}
+	}
+	return true
 }
--- go_set_test.go
+++ go_set_test.go
@@ -1,11 +1,20 @@
 package go_set
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestSet(t *testing.T) {
-  	s1 := Set{[]int{1, 1, 2, 2, 3}}
-	assert.Equal(t, len(s1.Elements()), 3)
+	s1 := NewSet([]int{1, 1, 2, 2, 3})
+	assert.Equal(t, len(s1.Val()), 3)
+
+	s2 := NewSet([]int{1, 4, 5, 4})
+	assert.Equal(t, (*s1).Equals(*s2), false)
+
+	s3 := NewSet([]int{1, 2, 3})
+	assert.Equal(t, (*s1).Equals(*s3), true)
+
+	s1.Update(*s2)
+	assert.Equal(t, len(s1.Val()), 5)
 }
